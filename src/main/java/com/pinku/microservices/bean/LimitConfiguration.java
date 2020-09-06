4da3e6c2c4a72d41406ddff19b2411b2e31c8692
Pinku <kpinku32@gmail.com>
2020-09-06 23:44:31 +0530

    limits-service repo

---
+++ src/main/java/com/pinku/microservices/bean/LimitConfiguration.java
@@ -0,0 +1,24 @@
+package com.pinku.microservices.bean;
+
+public class LimitConfiguration {
+	
+	private int maximum;
+	private int minimum;
+	
+	public LimitConfiguration() {
+		
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+}
---
+++ src/test/java/com/pinku/microservices/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinku.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
